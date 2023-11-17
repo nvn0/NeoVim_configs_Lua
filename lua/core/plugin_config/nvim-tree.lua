8afef1384e80ce151c9f62044bf2971dd3d12604
Nuno <75589883+nuno-src@users.noreply.github.com>
2023-11-17 01:42:12 +0000

    Update nvim-tree.lua

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -3,4 +3,4 @@ vim.g.loaded_newtrwPlugin = 1
 
 require("nvim-tree").setup()
 
-vim.keymap.set('n', '<c-n>' ':NvimTreeFindFileToggle<CR>')
+vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
