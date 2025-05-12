febf32db4a9393d9ff5ff01d81d7c22ba35c1d1b
Nuno <75589883+nvn0@users.noreply.github.com>
2025-05-13 00:27:59 +0100

    Update treesitter.lua

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 
-ensure_installed = {"c", "lua", "python", "bash", "rust", "javascript", "vim", "json", "xml", "c_sharp", "go"},
+ensure_installed = {"c", "lua", "python", "bash", "rust", "javascript", "vim", "json", "xml", "c_sharp", "go", "nim"},
 
   sync_install = false,
   auto_install = true,
